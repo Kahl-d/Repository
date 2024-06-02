64c4f756cda03711190224fda5f94f52736bc563
Khalid Khan <khalidkhan@Khalids-MacBook-Pro.local>
2024-06-02 05:45:21 +0530

    elements on landing page in place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './app.css';
-import LandingPage from './Components/LandingPage'; // Import LandingPage if it's part of App's structure
-import Home from './Components/Home'; // Import Home if it's part of App's structure
+import LandingPage from './Components/LandingPage'; 
+import Home from './Components/Home'; 
 
 const App = () => {
     return (
--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './lp.css'; 
 
 const LandingPage = () => {
     const navigate = useNavigate();
@@ -9,11 +10,18 @@ const LandingPage = () => {
     };
 
     return (
-        <div>
-            <h1>Landing Page</h1>
-            <button onClick={goToHome}>Go to Home</button>
+        <div id="landingContainer">
+            <h1 className='landingItem'>Welcome to TAEEP</h1>
+            <div id="loginSection" className='landingItem'>
+                <h2>Login</h2>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </div>
+            <button className='landingItem' onClick={goToHome}>Go to Home</button>
         </div>
     );
+
 }
 
 export default LandingPage;
