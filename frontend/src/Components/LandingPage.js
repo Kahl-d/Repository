890f4eadb3c9e053c06f991c0edddaec04fe2e73
Khalid Khan <khalidkhan@Khalids-MacBook-Pro.local>
2024-06-02 05:59:42 +0530

    backend frontend in sync, message going through

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './lp.css'; 
+import './lp.css';
 
 const LandingPage = () => {
     const navigate = useNavigate();
@@ -9,19 +9,38 @@ const LandingPage = () => {
         navigate('/home');
     };
 
+    const onLoginClick = () => {
+        fetch('http://127.0.0.1:5000/test') // Replace with your Flask backend endpoint
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle the response data here
+                console.log(data);
+            })
+            .catch((error) => {
+                // Handle any errors here
+                console.error('Error fetching data:', error);
+            });
+    }
+
+
     return (
         <div id="landingContainer">
             <h1 className='landingItem'>Welcome to TAEEP</h1>
-            <div id="loginSection" className='landingItem'>
+            <div className='landingItem'>
                 <h2>Login</h2>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button>Login</button>
+                <form id="loginSection">
+                    <input type="text" placeholder="Username" autoComplete='email' />
+                    <input type="password" placeholder="Password" autoComplete='current-password' />
+                    <button 
+                    onClick={onLoginClick}
+                    type="button">
+                        Login
+                    </button>
+                </form>
             </div>
             <button className='landingItem' onClick={goToHome}>Go to Home</button>
         </div>
     );
-
 }
 
 export default LandingPage;
